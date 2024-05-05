623a563af985cc76eaf55039cd338fc344fd3d71
ajmalcm <ajmalcm22@gmail.com>
2024-05-06 02:15:27 +0530

    still pending in loginRegister

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,110 +1,201 @@
-import React from 'react'
-import {Button, Container, Paper, TextField, Typography} from "@mui/material"
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography } from "@mui/material";
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import { useState } from "react";
+import VisualyHidden from "../styled/StyledComponents";
 
 const Login = () => {
+  const [isLogin, setisLogin] = useState(true);
+  const [loginData,setLoginData]=useState({userName:"",password:""});
+  const [registerData,setRegisterData]=useState({name:"",uname:"",pass:"",bio:""})
+  const {userName,password}=loginData;
+  const {name,uname,pass,bio}=registerData;
 
-  const [isLogin,setisLogin]=useState(true);
+  const loginChangeHandler=(e)=>{setLoginData({...loginData,[e.target.name]:e.target.value})}
+  const registerChangeHandler=(e)=>{setRegisterData({...registerData,[e.target.name]:e.target.value})}
+
+ const loginHandler=(e)=>{
+  e.preventDefault();
+ }
+
+ const registerHandler=(e)=>{
+  e.preventDefault();
+ }
+
+
 
   return (
-    <Container component={'main'} maxWidth='xs' sx={{
-      height:"100vh",
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-
-    }}>
-      <Paper
-      elevation={3}
+    <Container
+      component={"main"}
+      maxWidth="xs"
       sx={{
-        display:"flex",
-        flexDirection:"column",
-        alignItems:"center",
-        padding:4
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 4,
+        }}
       >
-      {
-        isLogin?
-        <>
-       <Typography variant='h5'>LOGIN</Typography>
-       <form style={{
-        width:"100%",
-        marginTop:"1rem"
-       }}>
-        <TextField
-          variant='outlined'
-          margin="normal"
-          required
-          fullWidth
-          label="userName"
-        />
-        <TextField
-          label="Password"
-          margin='normal'
-          required
-          fullWidth
-          variant='outlined'
-          type='password'
-        />
+        {isLogin ? (
+          <>
+            <Typography variant="h5">LOGIN</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+              onSubmit={loginHandler}
+            >
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="userName"
+                name="userName"
+                value={userName}
+                onChange={loginChangeHandler}
+              />
+              <TextField
+                label="Password"
+                margin="normal"
+                required
+                fullWidth
+                variant="outlined"
+                name="password"
+                value={password}
+                onChange={loginChangeHandler}
+                type="password"
+              />
 
-        <Button color='primary' variant='contained' sx={{marginTop:"1rem"}} type="submit" fullWidth>
-        LOGIN
-        </Button>
-        <Typography textAlign={'center'} margin={'1rem'}>OR</Typography>
-        <Button variant='text' onClick={()=>setisLogin(false)} fullWidth >
-          REGISTER
-        </Button>
-       </form>
-        </>
-        : <>
-       <Typography variant='h5'>REGISTER</Typography>
-       <form style={{
-        width:"100%",
-        marginTop:"1rem"
-       }}>
-       <TextField
-          variant='outlined'
-          margin="normal"
-          required
-          fullWidth
-          label="Name"
-        />
-        <TextField
-          variant='outlined'
-          margin="normal"
-          required
-          fullWidth
-          label="Bio"
-        />
-        <TextField
-          variant='outlined'
-          margin="normal"
-          required
-          fullWidth
-          label="userName"
-        />
-        <TextField
-          label="Password"
-          margin='normal'
-          required
-          fullWidth
-          variant='outlined'
-          type='password'
-        />
+              <Button
+                color="primary"
+                variant="contained"
+                sx={{ marginTop: "1rem" }}
+                type="submit"
+                fullWidth
+              >
+                LOGIN
+              </Button>
+              <Typography textAlign={"center"} margin={"1rem"}>
+                OR
+              </Typography>
+              <Button
+                variant="text"
+                onClick={() => setisLogin(false)}
+                fullWidth
+              >
+                REGISTER
+              </Button>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">REGISTER</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+              onSubmit={registerHandler}
+            >
+            <Stack margin={"auto"} width={"10rem"} position={"relative"}>
+              <Avatar sx={{
+                width:"10rem",
+                height:"10rem",
+                objectFit:"contain"
+              }}/>
+              <IconButton sx={{
+                position:"absolute",
+                right:0,
+                bottom:0,
+                color:"white",
+                bgcolor:"rgba(0,0,0,0.5)",
+                ":hover":{
+                  bgcolor:"rgba(0,0,0,0.7)"
+                }
+              }}
+              component="label"
+              >
+                <>
+                  <CameraAltIcon/>
+                  <VisualyHidden type="file"/>
+                </>
+              </IconButton>
+            </Stack>
 
-        <Button color='primary' variant='contained' sx={{marginTop:"1rem"}} type="submit" fullWidth>
-        REGISTER
-        </Button>
-        <Typography textAlign={'center'} margin={'1rem'}>Already have an account</Typography>
-        <Button variant='text' onClick={()=>setisLogin(true)} fullWidth >
-          Login
-        </Button>
-       </form>
-        </>
-      }
+            
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Name"
+                name="name"
+                value={name}
+                onChange={registerChangeHandler}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Bio"
+                name="bio"
+                value={bio}
+                onChange={registerChangeHandler}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="userName"
+                name="uname"
+                value={uname}
+                onChange={registerChangeHandler}
+              />
+              <TextField
+                label="Password"
+                margin="normal"
+                required
+                fullWidth
+                variant="outlined"
+                type="password"
+                name="pass"
+                value={pass}
+                onChange={registerChangeHandler}
+              />
+
+              <Button
+                color="primary"
+                variant="contained"
+                sx={{ marginTop: "1rem" }}
+                type="submit"
+                fullWidth
+              >
+                REGISTER
+              </Button>
+              <Typography textAlign={"center"} margin={"1rem"}>
+                Already have an account
+              </Typography>
+              <Button variant="text" onClick={() => setisLogin(true)} fullWidth>
+                Login
+              </Button>
+            </form>
+          </>
+        )}
       </Paper>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ frontend/src/styled/StyledComponents.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { styled } from '@mui/material'
+
+const VisualyHidden=styled('input')({
+    border:0,
+    clip:"rect(0 0 0 0)",
+    height:1,
+    margin:-1,
+    overflow:"hidden",
+    padding:0,
+    position:"absolute",
+    width:1,
+    whiteSpace:"nowrap"
+
+})
+
+export default VisualyHidden;
